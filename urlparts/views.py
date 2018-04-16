9c67d9c1171461d47613d1f71eacb45af3b74657
LucasBer <lucas.berghs@gmail.com>
2018-04-16 15:58:51 +0200

    Second steps
    Poging tot verkrijgen van URL path

--- urlparts/views.py
+++ urlparts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def index(request, page_alias):
     args = {}
     args['current_path'] = request.META['HTTP_REFERER']
+    try:
+        active = Page.objects.get(page_alias=page_alias)
+        except Page.DoesNotExist:
+            raise Http404("Page does not exist")
     return render(request, 'index.html', {'url_path': args['current_path']})
-    
