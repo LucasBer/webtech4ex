b55507f929a59f548d1a8b48038a4e26b5c4baef
LucasBer <lucas.berghs@gmail.com>
2018-04-16 16:04:08 +0200

    Final Commit
    Code terug herschreven maar geen oplossing gevonden van hoe je URL path verkrijgt.

--- urlparts/views.py
+++ urlparts/views.py
@@ -5,8 +5,4 @@ from django.shortcuts import render
 def index(request, page_alias):
     args = {}
     args['current_path'] = request.META['HTTP_REFERER']
-    try:
-        active = Page.objects.get(page_alias=page_alias)
-        except Page.DoesNotExist:
-            raise Http404("Page does not exist")
     return render(request, 'index.html', {'url_path': args['current_path']})
